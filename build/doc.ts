0e1d2d43a9a5323c08871e5dac96ab37194b4eb9
liukun <919590347@qq.com>
2020-07-21 21:56:28 +0800

    feat: 完善生成md, 准备开始做commit规范

--- build/doc.ts
+++ build/doc.ts
@@ -1,12 +1,27 @@
 
-const jsdoc2md = require('jsdoc-to-markdown')
-const glob = require('glob')
-const fs = require('fs')
+// const jsdoc2md = require('jsdoc-to-markdown')
+// const glob = require('glob')
+// const fs = require('fs')
+import jsdoc2md from 'jsdoc-to-markdown'
+import glob from 'glob'
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs'
+import { removeSync } from 'fs-extra'
+
+interface State {
+    docDir: string,
+    entries: string[],
+}
+
+interface Sidebar {
+    title: string,
+    path: string,
+    collapsable: boolean,
+}
 
 class Docs {
-    state={
-        file: '',
-        entries: glob.sync('src/!(_)*/*.ts')
+    state:State={
+        docDir: 'docs/src',
+        entries: glob.sync('src/!(_)*/!(_)*.ts') || [],
     }
 
     getMD(path: string) {
@@ -18,17 +33,52 @@ class Docs {
         })
     }
 
-    render() {
-        console.log('this.state.entries :>> ', this.state.entries);
-        const { entries } =  this.state
-        entries.forEach(async path=> {
-            const name = path.split('/')[1].split('.')[0]
-            try {
-                const data = await this.getMD(path)
-                fs.writeFileSync(`docs/src/${name}.md`, data)
-            } catch(e) {
-                console.log('getMD err :>> ', e);
+    setSidebar(sidebar: Sidebar[]) {
+        const path = 'docs/.vuepress/config.js'
+        const config: string = readFileSync(path, 'utf-8')
+        let result = eval(config)
+
+        result.themeConfig.sidebar = sidebar
+        result = JSON.stringify(result, null, 4)
+        writeFileSync(path, `module.exports = ${result}`, 'utf-8')
+    }
+
+    setMD() {
+        let { entries, docDir } =  this.state
+        console.log('entries :>> ', entries)
+
+        if(entries.length===0) return
+
+        removeSync(docDir)
+
+        return entries.map(async path=> {
+            let [dir, ,name] = path.split('/')
+            name = name.split('.')[0]
+            console.log('dir, name :>> ', dir, name)
+            
+            const data = await this.getMD(path)
+
+            if(!existsSync(docDir)) mkdirSync(docDir)
+            writeFileSync(`${docDir}/${name}.md`, data)
+
+            const sidebarItem = {
+                title: name,
+                collapsable: false,
+                path: `/${dir}/${name}/`
             }
+            return sidebarItem
+        })
+    }
+
+
+   render() {
+        const sidebar = this.setMD() || []
+
+        Promise.all(sidebar).then(data=> {
+            console.log('sidebar :>> ', data);
+            this.setSidebar(data)
+        }).catch(err=> {
+            console.log('err :>> ', err);
         })
     }
 }
