ab0927b106d86865172d680d40594bd25b47fddf
liukun <919590347@qq.com>
2020-09-03 01:03:41 +0800

    ci: 脚本调试

--- scripts/mv-coverage.sh
+++ scripts/mv-coverage.sh
@@ -3,6 +3,7 @@
 set -e
 
 pwd
-ls
-# rm -rf ./docs/.vuepress/dist/coverage
-# mv ./docs/.vuepress/coverage ./docs/.vuepress/dist
+ls ./docs/.vuepress
+ls ./docs/.vuepress/dist
+rm -rf ./docs/.vuepress/dist/coverage
+mv ./docs/.vuepress/coverage ./docs/.vuepress/dist
