c6258d80b4700373f21ba5e994a11adae7affaec
liukun <919590347@qq.com>
2020-09-02 23:27:37 +0800

    test: 测试用例描述统一包裹

--- src/toFirstCase/_toFirstCase.test.ts
+++ src/toFirstCase/_toFirstCase.test.ts
@@ -1,17 +1,13 @@
 import { toFirstCase } from './toFirstCase'
 
-describe('toFirstCase 全是大写', () => {
-    test('execute WORD', () => {
+describe('toFirstCase 方法测试', () => {
+    test('WORD => Word', () => {
         expect(toFirstCase('WORD')).toBe('Word')
     })
-})
-describe('toFirstCase 局部大写', () => {
-    test('execute wOrd', () => {
+    test('wOrd => Word', () => {
         expect(toFirstCase('wOrd')).toBe('Word')
     })
-})
-describe('toFirstCase 全是小写', () => {
-    test('execute word', () => {
+    test('word => Word', () => {
         expect(toFirstCase('word')).toBe('Word')
     })
 })
