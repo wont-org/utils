059b699f01dadac6e62fb2d2020e9e22c555442b
lk0606 <43233310+lk0606@users.noreply.github.com>
2020-10-29 21:29:15 +0800

    test: 简单兼容travis服务器本地时间 (#20)
    * feat(build refactor, test, new methods(validator), deps): 默认导出, 构建产物dekko测试, 添加校验方法, create模板更新
    
    1. 每个文件必须使用export default
     2. 新增validator校验方法
     3. 新增globalSetup测试打包产物
     4.
    生成build-stat.json便于统计及其他地方使用
     5. babel现在不做兼容转换, 是否转换取决于使用者
     6. create模板更新
    
    * chore: 打tag前先同步远程tag, 打alpha.1tag
    
    * docs(readme): 更新根目录下readme
    
    * chore(release): 0.0.1-alpha.2
    
    * chore: 分离push tag script
    
    * fix(import/export): 调整导入导出方式, 确保.d.ts正确提示, 优化globalSetup
    
    1. 调整create模板适应导入导出
    2.调整utils,强制一个文件只能export function
    3.优化globalSetup
    
    fix #4
    
    * chore(release): 0.0.1-alpha.3
    
    * fix: 入口文件应使用export default
    
    * chore: changelog全部生成
    
    * chore: changelog -s
    
    * docs: vuepress config
    
    * chore(release): 0.0.1-alpha.4
    
    * chore: changelog -s
    
    * docs: vuepress config path
    
    * ci: gh-pages skip_cleanup=false
    
    * ci: gh-pages deploy branch add dev
    
    * ci: travis test branch
    
    * ci: travis use dev
    
    * ci: travis skip clean
    
    * chore: unpkg
    
    * chore: create模板更新邮件
    
    * feat: 新增首字母大写方法
    
    * docs: 补充woRd使用说明
    
    * chore: 文档生成改造, api单独分组
    
    * feat: 新增get方法
    
    * test: 测试用例描述统一包裹
    
    * docs: 修正get默认值说明
    
    * docs: 文档添加测试覆盖率入口
    
    * ci: 移动生成的coverage
    
    * ci: 移动coverage
    
    * ci: 先删除coverage在移动
    
    * ci: do not set e
    
    * ci: scripts
    
    * ci: 直接在script写sh
    
    * ci: 脚本调试
    
    * ci: 脚本调试
    
    * chore: pkg scripts修改
    
    * chore(release): 0.0.1-alpha.5
    
    * chore(release): 0.0.1-alpha.6
    
    * ci: travis取消分支限制
    
    * chore(release): 0.0.1-alpha.7
    
    * feat: 增加身份证校验方法
    
    * docs: 增加jest单文件测试说明
    
    * feat: 增加邮箱校验方法
    
    * feat: 增加银行卡泛校验方法
    
    * feat: 增加中文校验方法
    
    * docs: 修正isCN文档注释
    
    * feat: 增加中文名校验方法
    
    * chore: prettier美化规范
    
    * feat: 合并验证方法
    
    * feat: 删除单个验证方法, 合成validator, type严格约束
    
    * docs: 验证器文档注释精确
    
    * feat: 新增座机校验
    
    * docs: 文档生成调整
    
    * chore(release): 0.0.1-alpha.8
    
    * feat: 提交CHANGELOG
    
    * docs: 增加unpkg地址
    
    * docs: 增加生态友链及unpkg说明
    
    * docs: unpkg不声明版本,自动获取latest
    
    * feat: 增加getDataType,获取数据类型
    
    * docs: 添加发版文档说明
    
    * chore(release): 0.0.1-alpha.9
    
    * chore: 增加--tags形式
    
    * build: 注释不必要的打包配置
    
    * feat: 新增formatTime方法
    
    * docs: formatTime文档配置更新, changelog上个版本更新
    
    * chore(release): 0.0.1-alpha.10
    
    * test: 简单兼容travis服务器本地时间

--- src/formatTime/_formatTime.test.ts
+++ src/formatTime/_formatTime.test.ts
@@ -4,11 +4,17 @@ describe('formatTime 方法测试', () => {
     const testCase = [
         [[1603809694212], '2020-10-27'],
         [[1603809694212, 'yyyy/MM/dd'], '2020/10/27'],
-        [[1603809694212, 'yyyy-MM-dd hh:mm:ss'], '2020-10-27 22:41:34'],
-        [[1603809694212, 'yyyy-MM-dd h:m:s'], '2020-10-27 22:41:34'],
+        [
+            [1603809694212, 'yyyy-MM-dd hh:mm:ss'],
+            '2020-10-27 22:41:34' || '2020-10-27 14:41:34',
+        ],
+        [
+            [1603809694212, 'yyyy-MM-dd h:m:s'],
+            '2020-10-27 22:41:34' || '2020-10-27 14:41:34',
+        ],
     ]
+    type Params = [number, string]
     test.each(testCase)('formatTime(%p)', (params, result) => {
-        const [a, b] = params as [number, string]
-        expect(formatTime(a, b)).toEqual(result)
+        expect(formatTime(...(params as Params))).toEqual(result)
     })
 })
