e17f60728ca377b46c608189355e1fd7376a3bf5
liukun <919590347@qq.com>
2020-11-23 22:55:01 +0800

    docs: 修改compose的jsdoc

--- src/compose/compose.ts
+++ src/compose/compose.ts
@@ -1,8 +1,8 @@
 /**
  * @description 函数组合。将两个或两个以上的函数组合生成一个新函数，新函数会按照栈顺序执行传入函数
  * @function compose
- * @param {Fn[]} fns - 函数组成的数组
- * @returns {Next} 返回一个执行函数，可传入需要处理的参数
+ * @param {Array.Function} funcs - 函数组成的数组
+ * @returns {Function} 返回一个执行函数，可传入需要处理的参数
  * @author liukun <919590347@qq.com>
  * @example
  * import { compose } from '@wont/utils'
@@ -13,8 +13,7 @@
  * getResult('a,b,c ') // returns [a, b, c]
  */
 
-type Next = (param: any) => any
 type Fn = (...rest: any[]) => any
-export function compose(fns: Fn[]): Next {
-    return (param) => fns.reduce((arg, fn) => fn(arg), param)
+export function compose(funcs: Function[]): Fn {
+    return (param) => funcs.reduce((arg, fn) => fn(arg), param)
 }
