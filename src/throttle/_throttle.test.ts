963152f344812925025f740873d2e7a3b65e7f0e
lk0606 <43233310+lk0606@users.noreply.github.com>
2020-11-25 20:42:48 +0800

    feat: 增加throttle节流参数 (#30)

---
+++ src/throttle/_throttle.test.ts
@@ -0,0 +1,22 @@
+import sinon from 'sinon'
+import { throttle } from './throttle'
+
+describe('throttle 方法测试', () => {
+    let clock
+    beforeEach(() => {
+        clock = sinon.useFakeTimers()
+    })
+    afterEach(() => {
+        clock.restore()
+    })
+    test('<=300ms called 3 times, but only first one success', () => {
+        const mockFn = jest.fn()
+        const throttled = throttle(mockFn)
+        for (let i = 0; i < 3; i += 1) {
+            clock.tick(100)
+            throttled(i)
+        }
+        expect(mockFn).toHaveBeenCalledTimes(1)
+        expect(mockFn.mock.calls[0][0]).toBe(0)
+    })
+})
