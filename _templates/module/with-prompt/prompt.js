6f11dec56ec599a8019dbd3c265b02e393df0080
liukun <919590347@qq.com>
2024-05-08 21:34:08 +0800

    chore: npm run create 提示

--- _templates/module/with-prompt/prompt.js
+++ _templates/module/with-prompt/prompt.js
@@ -4,12 +4,12 @@ module.exports = [
   {
     type: 'input',
     name: 'name',
-    message: '请输入模块名称，请使用驼峰式命名法（Camel-Case）',
+    message: '请输入模块名称，请使用驼峰式命名法（camelCase）',
     validate(val) {
       if (/^[a-zA-Z_][0-9a-zA-Z_]*$/.test(val)) {
         return true
       }
-      return '驼峰命名，只允许输入字母、数字、下划线'
+      return '小驼峰命名，只允许输入字母、数字、下划线'
     },
   },
 ]
