9ce98be00a497305d11492de1f66ca1fb14e01fa
Lk(刘坤) <lk.liu@eclicktech.com.cn>
2025-03-28 19:23:53 +0800

    chore: deps

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,220 +1,219 @@
 module.exports = {
-  title: '@wont/utils 函数库',
-  description:
-    '@wont/utils is javascript library, written with typescript. supporting browser and node',
-  base: '/utils/',
-  themeConfig: {
-    repo: 'https://github.com/wont-org/utils',
-    lastUpdated: '最后更新时间',
-    nav: [
+  "title": "@wont/utils 函数库",
+  "description": "@wont/utils is javascript library, written with typescript. supporting browser and node",
+  "base": "/utils/",
+  "themeConfig": {
+    "repo": "https://github.com/wont-org/utils",
+    "lastUpdated": "最后更新时间",
+    "nav": [
       {
-        text: '快速开始',
-        link: '/common/quickstart',
+        "text": "快速开始",
+        "link": "/common/quickstart"
       },
       {
-        text: '开发指南',
-        link: '/common/guide',
+        "text": "开发指南",
+        "link": "/common/guide"
       },
       {
-        text: '变更历史',
-        link: '/common/CHANGELOG',
+        "text": "变更历史",
+        "link": "/common/CHANGELOG"
       },
       {
-        text: '测试覆盖率',
-        link: '/coverage/lcov-report/index',
+        "text": "测试覆盖率",
+        "link": "/coverage/lcov-report/index"
       },
       {
-        text: '其他个人作品',
-        items: [
+        "text": "其他个人作品",
+        "items": [
           {
-            text: 'react组件库',
-            link: 'https://wont-org.github.io/biz-ui',
+            "text": "react组件库",
+            "link": "https://wont-org.github.io/biz-ui"
           },
           {
-            text: 'eslint-plugin-vue3-jsx',
-            link: 'https://github.com/lk0606/lint/tree/master/packages/eslint-plugin-vue3-jsx',
+            "text": "eslint-plugin-vue3-jsx",
+            "link": "https://github.com/lk0606/lint/tree/master/packages/eslint-plugin-vue3-jsx"
           },
           {
-            text: '库打包工具',
-            link: 'https://github.com/wont-org/tools',
+            "text": "库打包工具",
+            "link": "https://github.com/wont-org/tools"
           },
           {
-            text: 'antd可拖拽表格',
-            link: 'https://github.com/wont-org/use-antd-resizable-header',
+            "text": "antd可拖拽表格",
+            "link": "https://github.com/wont-org/use-antd-resizable-header"
           },
           {
-            text: 'vue3组件库',
-            link: 'https://github.com/wont-org/vue3-el',
+            "text": "vue3组件库",
+            "link": "https://github.com/wont-org/vue3-el"
           },
           {
-            text: 'vue3后台模板',
-            link: 'https://github.com/wont-org/v3-backend-tpl',
+            "text": "vue3后台模板",
+            "link": "https://github.com/wont-org/v3-backend-tpl"
           },
           {
-            text: 'react脚手架',
-            link: 'https://github.com/wont-org/cli',
-          },
-        ],
-      },
+            "text": "react脚手架",
+            "link": "https://github.com/wont-org/cli"
+          }
+        ]
+      }
     ],
-    sidebar: [
+    "sidebar": [
       {
-        title: '变更历史',
-        collapsable: false,
-        path: '/common/CHANGELOG',
+        "title": "变更历史",
+        "collapsable": false,
+        "path": "/common/CHANGELOG"
       },
-      '/common/code-commenting',
-      '/common/guide',
-      '/common/jest',
-      '/common/quickstart',
-      '/common/recommend',
-      '/common/z-refer',
+      "/common/code-commenting",
+      "/common/guide",
+      "/common/jest",
+      "/common/quickstart",
+      "/common/recommend",
+      "/common/z-refer",
       {
-        title: '函数API',
-        sidebarDepth: 0,
-        path: '',
-        collapsable: true,
-        children: [
+        "title": "函数API",
+        "sidebarDepth": 0,
+        "path": "",
+        "collapsable": true,
+        "children": [
           {
-            title: 'arrayToObject',
-            collapsable: false,
-            path: '/src/arrayToObject',
+            "title": "arrayToObject",
+            "collapsable": false,
+            "path": "/src/arrayToObject"
           },
           {
-            title: 'base64',
-            collapsable: false,
-            path: '/src/base64',
+            "title": "base64",
+            "collapsable": false,
+            "path": "/src/base64"
           },
           {
-            title: 'compose',
-            collapsable: false,
-            path: '/src/compose',
+            "title": "compose",
+            "collapsable": false,
+            "path": "/src/compose"
           },
           {
-            title: 'convertObjKeys',
-            collapsable: false,
-            path: '/src/convertObjKeys',
+            "title": "convertObjKeys",
+            "collapsable": false,
+            "path": "/src/convertObjKeys"
           },
           {
-            title: 'curry',
-            collapsable: false,
-            path: '/src/curry',
+            "title": "curry",
+            "collapsable": false,
+            "path": "/src/curry"
           },
           {
-            title: 'debounce',
-            collapsable: false,
-            path: '/src/debounce',
+            "title": "debounce",
+            "collapsable": false,
+            "path": "/src/debounce"
           },
           {
-            title: 'findListDuplicatesByKeyList',
-            collapsable: false,
-            path: '/src/findListDuplicatesByKeyList',
+            "title": "findListDuplicatesByKeyList",
+            "collapsable": false,
+            "path": "/src/findListDuplicatesByKeyList"
           },
           {
-            title: 'formatOptions',
-            collapsable: false,
-            path: '/src/formatOptions',
+            "title": "formatOptions",
+            "collapsable": false,
+            "path": "/src/formatOptions"
           },
           {
-            title: 'formatTime',
-            collapsable: false,
-            path: '/src/formatTime',
+            "title": "formatTime",
+            "collapsable": false,
+            "path": "/src/formatTime"
           },
           {
-            title: 'formatTree',
-            collapsable: false,
-            path: '/src/formatTree',
+            "title": "formatTree",
+            "collapsable": false,
+            "path": "/src/formatTree"
           },
           {
-            title: 'formatTreeSelectProps',
-            collapsable: false,
-            path: '/src/formatTreeSelectProps',
+            "title": "formatTreeSelectProps",
+            "collapsable": false,
+            "path": "/src/formatTreeSelectProps"
           },
           {
-            title: 'get',
-            collapsable: false,
-            path: '/src/get',
+            "title": "get",
+            "collapsable": false,
+            "path": "/src/get"
           },
           {
-            title: 'getDataType',
-            collapsable: false,
-            path: '/src/getDataType',
+            "title": "getDataType",
+            "collapsable": false,
+            "path": "/src/getDataType"
           },
           {
-            title: 'getSelectOptionsBy',
-            collapsable: false,
-            path: '/src/getSelectOptionsBy',
+            "title": "getSelectOptionsBy",
+            "collapsable": false,
+            "path": "/src/getSelectOptionsBy"
           },
           {
-            title: 'getUrlParam',
-            collapsable: false,
-            path: '/src/getUrlParam',
+            "title": "getUrlParam",
+            "collapsable": false,
+            "path": "/src/getUrlParam"
           },
           {
-            title: 'getUrlParams',
-            collapsable: false,
-            path: '/src/getUrlParams',
+            "title": "getUrlParams",
+            "collapsable": false,
+            "path": "/src/getUrlParams"
           },
           {
-            title: 'getUrlSearch',
-            collapsable: false,
-            path: '/src/getUrlSearch',
+            "title": "getUrlSearch",
+            "collapsable": false,
+            "path": "/src/getUrlSearch"
           },
           {
-            title: 'isFileName',
-            collapsable: false,
-            path: '/src/isFileName',
+            "title": "isFileName",
+            "collapsable": false,
+            "path": "/src/isFileName"
           },
           {
-            title: 'locale',
-            collapsable: false,
-            path: '/src/locale',
+            "title": "locale",
+            "collapsable": false,
+            "path": "/src/locale"
           },
           {
-            title: 'LRUCache',
-            collapsable: false,
-            path: '/src/LRUCache',
+            "title": "LRUCache",
+            "collapsable": false,
+            "path": "/src/LRUCache"
           },
           {
-            title: 'objectToFormData',
-            collapsable: false,
-            path: '/src/objectToFormData',
+            "title": "objectToFormData",
+            "collapsable": false,
+            "path": "/src/objectToFormData"
           },
           {
-            title: 'set',
-            collapsable: false,
-            path: '/src/set',
+            "title": "set",
+            "collapsable": false,
+            "path": "/src/set"
           },
           {
-            title: 'throttle',
-            collapsable: false,
-            path: '/src/throttle',
+            "title": "throttle",
+            "collapsable": false,
+            "path": "/src/throttle"
           },
           {
-            title: 'toFirstCase',
-            collapsable: false,
-            path: '/src/toFirstCase',
+            "title": "toFirstCase",
+            "collapsable": false,
+            "path": "/src/toFirstCase"
           },
           {
-            title: 'validator',
-            collapsable: false,
-            path: '/src/validator',
+            "title": "validator",
+            "collapsable": false,
+            "path": "/src/validator"
           },
           {
-            title: 'winOpen',
-            collapsable: false,
-            path: '/src/winOpen',
-          },
-        ],
-      },
-    ],
+            "title": "winOpen",
+            "collapsable": false,
+            "path": "/src/winOpen"
+          }
+        ]
+      }
+    ]
   },
-  head: [
+  "head": [
     [
-      'script',
+      "script",
       {
-        src: 'https://unpkg.com/@wont/utils',
-      },
-    ],
-  ],
+        "src": "https://unpkg.com/@wont/utils"
+      }
+    ]
+  ]
 }
